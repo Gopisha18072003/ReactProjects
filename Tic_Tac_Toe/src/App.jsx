3990da45925ed8acdf2d0bf719e57ce9844fd9a8
Gopisha18072003 <gopishaw111@gmail.com>
2024-02-03 14:51:29 +0530

    added tic tac toe project

---
+++ Tic_Tac_Toe/src/App.jsx
@@ -0,0 +1,34 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import { useState } from "react";
+function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  function handleSelectedSquare() {
+    setActivePlayer(activePlayer === "X" ? "O" : "X");
+  }
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectedSquare}
+          activePlayerSymbol={activePlayer}
+        />
+      </div>
+      LOG
+    </main>
+  );
+}
+
+export default App;
